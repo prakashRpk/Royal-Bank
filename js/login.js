487e3afec6033fd6992629d8a20eff8f66ca32d2
Rpkprakash <prakashrprakashr127@gmail.com>
2024-12-15 19:21:57 +0530

    commit changes

---
+++ js/login.js
@@ -0,0 +1,7 @@
+import { details } from "./details"
+var det = details
+
+function Sub(){
+    console.log("hi")
+}
+Sub()
---
+++ js/module.js
@@ -0,0 +1,18 @@
+let F_name = document.querySelector(".Fname"),
+L_name = document.querySelector(".Lname"),
+P_Num = document.querySelector(".Num"),
+Gmail = document.querySelector(".Gmail"),
+Aadhar_num = document.querySelector(".Anum"),
+PAN_card = document.querySelector(".PAN"),
+Address = document.querySelector(".Address")
+
+export let details = {
+    firstname : F_name,
+    lastname : L_name,
+    phonenumber : P_Num,
+    gmail : Gmail
+}
+
+
+
+
